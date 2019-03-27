898987d251ab241df8f3977cf663507d72e81cf9
Dabas, Naveen <naveen.usit@ymail.com>
2019-03-27 15:45:59 +0100

    [TA-1] added sorting tests.

--- src/test/java/com/tripactions/game/model/HandTest.java
+++ src/test/java/com/tripactions/game/model/HandTest.java
@@ -1,6 +1,9 @@
 package com.tripactions.game.model;
 
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import static org.junit.Assert.*;
 
 public class HandTest {
@@ -42,5 +45,23 @@ public class HandTest {
   @Test
   public void toStringTest() {
     assertEquals(thatHand.toString(), "<hand [5C, 5S, 5D, 5H, 7S], FOUR_OF_A_KIND>");
+  }
+
+  @Test
+  public void sortOrderByHandTypeTest() {
+    Hand flush = Hand.fromString("7S, 5C, 5H, 5D, 5S");
+    Hand onePair = Hand.fromString("KS, KC, 5S, 6S, 7S");
+    Hand twoPair = Hand.fromString("KS, KC, 5S, 5C, 6D");
+    List<Hand> orderedList = List.of(onePair, twoPair, flush);
+    List<Hand> unorderedList = List.of(twoPair, flush, onePair);
+
+    List<Hand> testList = new ArrayList<>();
+    testList.add(twoPair);
+    testList.add(flush);
+    testList.add(onePair);
+    Collections.sort(testList);
+
+    assertEquals(testList, orderedList);
+    assertNotEquals(testList, unorderedList);
   }
 }
